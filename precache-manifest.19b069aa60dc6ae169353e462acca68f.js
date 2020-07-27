d76032622b52bba917a686a9af6011dac4041ace
MythriJL <mythri1020@gmail.com>
2020-07-27 21:46:49 +0530

    Updates

---
+++ precache-manifest.19b069aa60dc6ae169353e462acca68f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c98a7ab6bf1d264c89f6fbce5c201d",
+    "url": "/Library_template/index.html"
+  },
+  {
+    "revision": "ea1d678f0feb4ae74b9d",
+    "url": "/Library_template/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2228f4cf748f5ec4f654",
+    "url": "/Library_template/static/css/main.509c4836.chunk.css"
+  },
+  {
+    "revision": "ea1d678f0feb4ae74b9d",
+    "url": "/Library_template/static/js/2.f4a7fbdd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Library_template/static/js/2.f4a7fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2228f4cf748f5ec4f654",
+    "url": "/Library_template/static/js/main.9dc5bdc3.chunk.js"
+  },
+  {
+    "revision": "ade5492b342105161cbf",
+    "url": "/Library_template/static/js/runtime-main.4ca8f9e9.js"
+  },
+  {
+    "revision": "362e3efe4b7b1e4c3e562b8d6fb44f83",
+    "url": "/Library_template/static/media/home-about.362e3efe.png"
+  },
+  {
+    "revision": "541d13ca9684a61cdaf7ad057713f6b3",
+    "url": "/Library_template/static/media/home.541d13ca.jpeg"
+  },
+  {
+    "revision": "d570712b0ffe741241b3837cfcace14c",
+    "url": "/Library_template/static/media/nav-logo.d570712b.jpg"
+  }
+]);
