b4998e589dad47f80ab08f99b7ad4c724a7944a4
Mythri J L <mythri1020@gmail.com>
2021-09-07 14:16:48 +0530

    aligned learning resources page

--- src/routes/Learning/index.js
+++ src/routes/Learning/index.js
@@ -14,75 +14,111 @@ import Ejournals from './ejournals.jpg'
 import Moodle from './Moodle.jpeg';
 import Database from './database.png';
 import Digimat from './digimat.jpeg';
-import {Link,} from "react-router-dom";
-import {Image } from 'react-bootstrap'
+import { Link, } from "react-router-dom";
+import { Image } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap';
 
 import './index.css';
 
 export class Learning extends React.Component {
 
     render() {
-      return (
+        return (
             <>
-        <div className="head">
-                <h1>Learning Resources </h1>
-            </div>
-       <br></br>
-       
-       <Link to="learning/classroom">
-     <Image src={Classroom} alt = "Classroom" className ="home-learning-image" fluid/>
-     </Link>
-     
-     <Link to="/learning/cd">
-     <Image src={Cds} alt = "Cds" className ="home-learning-image" fluid />
-     </Link>
+                <div className="head">
+                    <h1>Learning Resources </h1>
+                </div>
+                <br></br>
+                <Container>
+                    <Row>
 
 
-     <a href= 'https://www.knimbus.com/'>
-     <Image src={Knimbus} alt = "Knimbus" className ="home-learning-image"fluid /> 
-     </a>
-     
-     <a href= 'https://nptel.ac.in/'>
-     <Image src={Nptl} alt = "Nptl" className ="home-learning-image"fluid />
-     </a>
-     
-     <a href= 'https://duraspace.org/dspace/'>
-     <Image src={Dspace} alt = "Dspace" className ="home-learning-image"fluid />
-     </a>
-
-     <Link to="/learning/link">
-     <Image src={link} alt = "Link" className ="home-learning-image"fluid />
-     </Link>
-
-     <Link to="learning/ebook">
-     <Image src={Ebook} alt = "Ebook" className ="home-learning-image"fluid />
-     </Link>
-
-     <a href= 'https://ndl.iitkgp.ac.in/'>
-     <Image src={National} alt = "National" className ="home-learning-image"fluid />
-     </a>
-
-     <a href= "https://opac.app/en/">
-     <Image src={Opac} alt = "Opac" className ="home-learning"fluid />
-     </a>
-
-     <Link to="learning/ejournal">
-     <Image src={Ejournals} alt = "Ejournals" className ="home-learning-image"fluid />
-     </Link>
-
-     <a href= 'https://moodle.org/'>
-     <Image src={Moodle} alt = "Moodle" className ="home-learning-image"fluid />
-     </a>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <Link to="learning/classroom">
+                                <Image src={Classroom} alt="Classroom" className="home-learning-image" fluid />
+                            </Link>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
 
 
-     <a href= 'https://www.digimat.in/'>
-     <Image src={Digimat} alt = "digimat" className ="home-learning-image"fluid />
-     </a>
+                            <a href='https://www.digimat.in/'>
+                                <Image src={Digimat} alt="digimat" className="home-learning-image" fluid />
+                            </a>
+
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <Link to="/learning/cd">
+                                <Image src={Cds} alt="Cds" className="home-learning-image" fluid />
+                            </Link>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <a href='https://moodle.org/'>
+                                <Image src={Moodle} alt="Moodle" className="home-learning-image" fluid />
+                            </a>
+
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <a href='https://www.knimbus.com/'>
+                                <Image src={Knimbus} alt="Knimbus" className="home-learning-image" fluid />
+                            </a>
+
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <a href='https://nptel.ac.in/'>
+                                <Image src={Nptl} alt="Nptl" className="home-learning-image" fluid />
+                            </a>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+
+                            <a href='https://duraspace.org/dspace/'>
+                                <Image src={Dspace} alt="Dspace" className="home-learning-image" fluid />
+                            </a>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <Link to="/learning/link">
+                                <Image src={link} alt="Link" className="home-learning-image" fluid />
+                            </Link>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+
+                            <Link to="learning/ebook">
+                                <Image src={Ebook} alt="Ebook" className="home-learning-image" fluid />
+                            </Link>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <a href='https://ndl.iitkgp.ac.in/'>
+                                <Image src={National} alt="National" className="home-learning-image" fluid />
+                            </a>
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+                            <a href="https://opac.app/en/">
+                                <Image src={Opac} alt="Opac" className="home-learning" fluid />
+                            </a>
+
+                        </Col>
+                        <Col lg={4} md={12} sm={12} xs={12} className='set1' >
+
+                            <Link to="learning/ejournal">
+                                <Image src={Ejournals} alt="Ejournals" className="home-learning-image" fluid />
+                            </Link>
+                        </Col>
+                        
 
 
-  </>         
+
+
+                    </Row>
+
+                </Container>
+
+
+
+
+
+
+            </>
         )
-        
+
     }
 }
 export default Learning;
