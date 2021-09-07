5d5d5b8ae9697858a08a5ef29057781b5a9f66d1
Mythri J L <mythri1020@gmail.com>
2021-09-07 14:06:12 +0530

    integrated api for classroom

--- src/routes/classroom/index.js
+++ src/routes/classroom/index.js
@@ -3,7 +3,17 @@ import './index.css';
 import { Row, Col, Container } from 'react-bootstrap';
 
 function Classroom() {
-   
+
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch("https://run.mocky.io/v3/fb135cac-d439-4fa6-9673-4ccca69212b6")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setItems(result.classroom)
+                },
+            )
+    }, [])
     return (
         <>
             <div className="head">
@@ -12,85 +22,23 @@ function Classroom() {
             <p><br /><br /></p>
             <Container>
                 <Row>
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+                    {
 
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
+                        items && items.map((x, idx) =>
 
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+                            <Col lg={4} md={12} sm={12} xs={12} className='set1' id="idx">
+                                <div className="embed-responsive embed-responsive-16by9">
+                                    <iframe className="embed-responsive-item" src={x.link} frameBorder='10' allowFullScreen />
+                                </div>
+                                <p className='description'>{x.subject}</p>
+                            </Col>
 
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
 
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+                        )
 
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
+                    }
                 </Row>
-
-                <Row>
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
-
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
-
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
-
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
-                </Row>
-
-                <Row>
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
-
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
-
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
-                    <Col lg={4} md={12} sm={12} xs={12} className='set1'>
-
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
-                        </div>
-                        <p className='description'>Applied Science</p>
-                    </Col>
-
-                </Row>
+               
             </Container>
 
         </>
