69f10de032d0704a9075b3424823a716c96853db
Mythri J L <mythri1020@gmail.com>
2021-09-08 16:57:51 +0530

    rectified about us page

--- src/routes/aboutus/index.js
+++ src/routes/aboutus/index.js
@@ -79,7 +79,8 @@ export class AboutUS extends React.Component {
                     </div>
                 
                 </Container>
-          
+                
+                
             </>       
         )
     }
